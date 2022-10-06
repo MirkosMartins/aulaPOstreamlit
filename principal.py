3829d359f11afcad3054e3fac69d80fadd7f87a7
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-10-06 19:27:25 -0300

    adicionando email

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 nome = st.text_input("Digite o seu nome:")
+email = st.text_input("Digite o seu email:")
 if st.button("Inserir"):
-    st.write("Ola ",nome," tudo bem?")
+    st.write("Ola ",nome," seu email eh:",email)
