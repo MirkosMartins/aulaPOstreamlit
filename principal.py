12243de8a9f687b2f39c4dbfd2c435a7c283217b
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-10-06 19:38:29 -0300

    correcao5

--- principal.py
+++ principal.py
@@ -7,6 +7,6 @@ email = st.text_input("Digite o seu email:")
 if st.button("Inserir"):
     novoReg = pd.DataFrame(data=[nome,email],columns=dados.columns)
     novo = pd.concat([dados,novoReg])
-    novo.to_csv("dados.csv")
+    novo.to_csv("dados.csv",index=False)
     st.table(novo)
     st.write("Ola ",nome," seu email eh:",email)
