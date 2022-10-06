6c5e8971e8f4fafadfd935bcf0a281b9982e4a83
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-10-06 19:46:23 -0300

    correcao7

--- principal.py
+++ principal.py
@@ -8,6 +8,9 @@ if st.button("Inserir"):
     #novoReg = pd.DataFrame(data=[nome,email],columns=dados.columns)
     #novo = pd.concat([dados,novoReg])
     #novo.to_csv("dados.csv",index=False)
-    dados.insert('nome':nome,'email':email)
+    #dados.insert('nome':nome,'email':email)
+    dados.loc[-1]=[nome,email]
+    dados.index = dados.index+1
+    dados = dados.sort_index()
     st.table(dados)
     st.write("Ola ",nome," seu email eh:",email)
